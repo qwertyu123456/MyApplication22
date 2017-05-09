03a7fae51948499293daaee0d8fe1a101f699fc8
lizhuofang <1072075887@qq.com>
2017-05-09 14:39:47 +0800

    hh

--- app/src/main/java/com/example/lizhuofang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lizhuofang/myapplication/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.lizhuofang.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView mText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mText= (TextView) findViewById(R.id.text_VIEW);
     }
 }
