307bd097fd980c26e7a266509dd0c3fa411e04f2
qwertyu123456 <1072075887@qq.com>
2017-05-09 15:46:20 +0800

    hh

---
+++ app/src/main/java/com/example/lizhuofang/myapplication/WelcomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.lizhuofang.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by lizhuofang on 2017/5/9.
+ */
+
+public class WelcomeActivity extends Activity {
+    private Handler mHandler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(WelcomeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },3000);
+    }
+}
